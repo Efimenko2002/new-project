5079c9058f6a5a4a0bb4d0253b22b3ea38a89460
Efimenko2002 <31364350+Efimenko2002@users.noreply.github.com>
2025-06-19 01:01:18 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'notemap-cache-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  'https://unpkg.com/leaflet/dist/leaflet.js',
+  'https://unpkg.com/leaflet/dist/leaflet.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(resp => resp || fetch(event.request))
+  );
+});
